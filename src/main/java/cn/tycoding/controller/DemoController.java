d2abae989cdfbdbdd5f4655acb9eddd1015f60cd
yuanjinxiugithub <1871022901@qq.com>
2019-01-05 14:56:23 +0800

    first init

---
+++ src/main/java/cn/tycoding/controller/DemoController.java
@@ -0,0 +1,34 @@
+package cn.tycoding.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/demo")
+public class DemoController {
+    
+    /*@Value(value = "${user.name}")
+    private String userName;
+    
+    @Value(value = "${user.age}")
+    private String userAge;*/
+    
+    @RequestMapping("/list")
+    public ModelAndView itemList(){
+        List<String> list = new ArrayList<>();
+        list.add("list-one");
+        list.add("list-two");
+        list.add("list-three");
+        list.add("list-four");
+      /*  list.add(userName);
+        list.add(userAge);*/
+        ModelAndView modelAndView = new ModelAndView("thymeleaf/demo01");
+        modelAndView.addObject("list", list);
+        return modelAndView;
+    }
+
+}
