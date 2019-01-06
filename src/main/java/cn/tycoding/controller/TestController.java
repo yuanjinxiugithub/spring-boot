848c1379e684b3cf94141891f5a68e8e1d76257d
yuanjinxiugithub <1871022901@qq.com>
2019-01-06 17:59:51 +0800

    add a method

--- src/main/java/cn/tycoding/controller/TestController.java
+++ src/main/java/cn/tycoding/controller/TestController.java
@@ -2,8 +2,14 @@ package cn.tycoding.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequestMapping("test")
 public class TestController {
+
+    @RequestMapping("sayHi")
+    public String sayHi(){
+        return "hi!";
+    }
 }
