221b24031824059042593c10a5d86e6846b3d10f
yuanjinxiugithub <1871022901@qq.com>
2019-01-06 19:48:18 +0800

    add a method

--- src/main/java/cn/tycoding/SpringbootApplication.java
+++ src/main/java/cn/tycoding/SpringbootApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
- *  启动程序 --
+ *  启动程序---
  */
 @SpringBootApplication
 public class SpringbootApplication {
